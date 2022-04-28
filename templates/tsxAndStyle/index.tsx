81c70612ff4e953b288430bd939ce0b5610f6328
Ümit Yaşar Kalkan <uykalkan@gmail.com>
2022-04-28 19:59:28 +0300

    react 18 uyumluluğu

--- templates/tsxAndStyle/index.tsx
+++ templates/tsxAndStyle/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 type __name__Props = {
-
+  children?: React.ReactNode
 }
 
 const __name__ : React.FC<__name__Props> = () => {
